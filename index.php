f5ef4043dabd9b6569ca6415ac850f6dd94a4174
Karylle Manrique <manriquekarylle891@gmail.com>
2022-05-16 02:29:39 +0800

    index

---
+++ index.php
@@ -0,0 +1,315 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <title>Filipino Arts Gallery</title>
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+
+    <link rel="stylesheet" href="assets/css/templatemo-hexashop.css">
+
+    <link rel="stylesheet" href="assets/css/owl-carousel.css">
+
+    <link rel="stylesheet" href="assets/css/lightbox.css">
+
+
+
+    </head>
+    
+    <body>
+    
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>  
+    <!-- ***** Preloader End ***** -->
+    
+ //dini na yung simula
+ //andini logo   
+    <!-- ***** Header Area Start ***** -->
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+                        <ul class="nav">
+
+                
+                            <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+                            <li class="submenu">
+                                <a href="javascript:;">Information</a>
+                                <ul>
+                                    <li><a href="about.php">About Us</a></li>
+                                    <li><a href="history.php">History</a></li>
+                                    <li><a href="creators.php">About Creators</a></li>
+                                    <li><a href="contact.php">Contact Us</a></li>
+                                </ul>
+                            </li>
+                            <li class="submenu">
+                                <a href="javascript:;">Artists</a>
+                                <ul>
+                                    <li><a href="artist1.php">Fernando Amorsolo</a></li>
+                                    <li><a href="artist2.php">Juan Luna</a></li>
+                                    <li><a href="artist3.php">Guillermo Tolentino</a></li>
+                                    <li><a href="artist4.php">Benedicto Cabrera</a></li>
+                                    <li><a href="artist5.php">Vicente Manansala</a></li>
+                                    <li><a href="artist6.php">Victorio Edades</a></li>
+                                    <li><a href="artist7.php">Ang Kiukok</a></li>
+                                    <li><a href="artist8.php">Napoleon Abueva</a></li>
+                                    <li><a href="artist9.php">Hernando Ocampo</a></li>
+                                    <li><a href="artist10.php">Pacita Abad</a></li>
+                                </ul>
+
+                        </ul>        
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                        <header class="text-black">
+                            <div class="container px-4 text-center">
+                                <h1 class="fw-bolder">FILIPINO ARTS GALLERY</h1>
+                                <p class="lead">"Find the art in your heart. It is the conversion of imagination to reality."</p>
+                            </div>
+                        </header>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                        <!-- ***** Menu End ***** -->
+
+
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
+
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+
+    <!-- ***** Main Banner Area Start ***** -->
+    <div class="main-banner" id="top">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="left-content">
+                        <div class="thumb">
+                            <div class="inner-content">
+
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <h4>Fernando Amorsolo</h4>
+                                <span>Known for his Illuminated Landscape Paintings</span>
+                                <div class="main-border-button">
+                                    
+                                </div>
+                            </div>
+                            <img src="assets/images/amorsolo.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="right-content">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="right-first-image">
+                                    <div class="thumb">
+                                        <div class="inner-content">
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                            <h4>Juan Luna</h4>
+                                        </div>
+                                        <img src="assets/images/luna.jpg">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-6">
+                                <div class="right-first-image">
+                                    <div class="thumb">
+                                        <div class="inner-content">
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                            <h4>Guillermo Tolentino</h4>
+                                        </div>
+                                        <img src="assets/images/Tolentino.jpg">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-6">
+                                <div class="right-first-image">
+                                    <div class="thumb">
+                                        <div class="inner-content">
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                            <h4>Benedicto Cabrera</h4>
+                                        </div>
+                                        <img src="assets/images/cabrera.jpg">
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div class="col-lg-6">
+                                <div class="right-first-image">
+                                    <div class="thumb">
+                                        <div class="inner-content">
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                            <h4>Vicente Manansala</h4>
+                                        </div>
+                                            <div class="inner">
+                                                <div class="main-border-button">
+                                                </div>
+                                            </div>
+
+                                        <img src="assets/images/Manansala.jpg">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ***** Main Banner Area End ***** -->
+    
+    <!-- ***** Footer Start ***** -->
+    <footer>
+                <div class="col-lg-12">
+                    <div class="under-footer">
+                        <p>Copyright © 2022 Filipino Arts Gallery. All Rights Reserved. 
+                        
+                        <br>Template Credits:<a href="https://templatemo.com" target="_parent" title="free css templates"> TemplateMo</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    
+    <!-- ***** Footer ends ***** -->
+    
+
+    <!-- jQuery -->
+    <script src="assets/js/jquery-2.1.0.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="assets/js/popper.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- Plugins -->
+    <script src="assets/js/owl-carousel.js"></script>
+    <script src="assets/js/accordions.js"></script>
+    <script src="assets/js/datepicker.js"></script>
+    <script src="assets/js/scrollreveal.min.js"></script>
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <script src="assets/js/imgfix.min.js"></script> 
+    <script src="assets/js/slick.js"></script> 
+    <script src="assets/js/lightbox.js"></script> 
+    <script src="assets/js/isotope.js"></script> 
+    
+    <!-- Global Init -->
+    <script src="assets/js/custom.js"></script>
+
+    <script>
+
+        $(function() {
+            var selectedClass = "";
+            $("p").click(function(){
+            selectedClass = $(this).attr("data-rel");
+            $("#portfolio").fadeTo(50, 0.1);
+                $("#portfolio div").not("."+selectedClass).fadeOut();
+            setTimeout(function() {
+              $("."+selectedClass).fadeIn();
+              $("#portfolio").fadeTo(50, 1);
+            }, 500);
+                
+            });
+        });
+
+    </script>
+
+  </body>
+</html>
