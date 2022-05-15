6e8bb597e3acf4ddf02ea834a93ad53ada49cfd2
Karylle Manrique <manriquekarylle891@gmail.com>
2022-05-16 02:41:34 +0800

    artist6

---
+++ artist6.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <title>Filipino Arts Gallery - About Page</title>
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+
+    <link rel="stylesheet" href="assets/css/templatemo-hexashop.css">
+
+    <link rel="stylesheet" href="assets/css/owl-carousel.css">
+
+    <link rel="stylesheet" href="assets/css/lightbox.css">
+
+    </head>
+    
+    <body>
+    
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>  
+    <!-- ***** Preloader End ***** -->
+    
+    
+    <!-- ***** Header Area Start ***** -->
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+
+
+                        <!-- ***** Menu Start ***** -->
+                        <ul class="nav">
+                            <li class="scroll-to-section"><a href="index.php" class="active">Home</a></li>
+                            <li class="submenu">
+                                <a href="javascript:;">Information</a>
+                                <ul>
+                                    <li><a href="about.php">About Us</a></li>
+                                    <li><a href="history.php">History</a></li>
+                                    <li><a href="creators.php">About Creators</a></li>
+                                    <li><a href="contact.php">Contact Us</a></li>
+                                </ul>
+                            </li>
+                            <li class="submenu">
+                                <a href="javascript:;">Artists</a>
+                                <ul>
+                                    <li><a href="artist1.php">Fernando Amorsolo</a></li>
+                                    <li><a href="artist2.php">Juan Luna</a></li>
+                                    <li><a href="artist3.php">Guillermo Tolentino</a></li>
+                                    <li><a href="artist4.php">Benedicto Cabrera</a></li>
+                                    <li><a href="artist5.php">Vicente Manansala</a></li>
+                                    <li><a href="artist6.php">Victorio Edades</a></li>
+                                    <li><a href="artist7.php">Ang Kiukok</a></li>
+                                    <li><a href="artist8.php">Napoleon Abueva</a></li>
+                                    <li><a href="artist9.php">Hernando Ocampo</a></li>
+                                    <li><a href="artist10.php">Pacita Abad</a></li>
+                                </ul>
+                        </ul>        
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
+
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+</div>
+        <div class="content-wrapper">
+            <div class="inner-container container">
+                <div class="row">
+                    <div class="section-header col-md-12">
+                        <h2>Victorio Edades</h2>
+                        <span>(1895-1985)</span>
+                        <br>
+                        <br>
+                        <br>
+                        <p> Victorio Edades was a revolutionary artist, who was the leader of the Thirteen Moderns, a group of artists who believed and promoted Modernism in artwork. A traveling exhibition from the New York Armory Hall was what inspired Edades to follow and appreciate modern art. Through the exhibition, he got inspired by some of the Modern European artists including Gaugin, Picasso and Matisse and felt a pull towards Modernism and Surrealism. He believed that Modernism allowed artists to experiment with art and encouraged artistic expression and freedom. One of his most famous paintings, The Sketch (1927) won the second prize in the Annual Exhibition of North American Artists. However, his ideas of modern art were not easily accepted in the Philippines. He had to struggle to bring a change in the conventions of domestic art in his nation. Nonetheless his contribution was recognized and he was bestowed with the honor of National Artists of the Philippines in Visual Arts (Painting) in 1976.</p>
+                        <br>
+                        <br>
+                    </div>
+                    <div class="projects-holder">
+                        <div class="row">
+                            <div class="col-md-4 project-item mix design">
+                                <div class="project-thumb">
+                                    <img src="assets/images/eda1.jpg" alt="">
+                                    <div class="overlay-b">
+                                        <div class="overlay-inner">
+                                            <a href="assets/images/eda1.jpg" class="fancybox fa fa-expand" title="The Builders (1928) "></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="box-content project-detail">
+                                    <h2><a href="project-details.php"> The Builders (1928) </a></h2>
+                                    <p>One of Victorio Edades' major works that he exhibited in 1928 during his one-man show. Edades executed this upon his return to the Philippines from studies in the United States. In his campaign for modernism, this is one of his paintings that shows his enhanced knowledge and skill in modern art. This artwork emphasized linear and structural composition above other pictorial elements, conveying the essence of men engaged in labor through the contortion of the bodies.</p>
+                                </div>
+                            </div> <!-- /.project-item -->
+                            <div class="col-md-4 project-item mix nature">
+                                <div class="project-thumb">
+                                    <img src="assets/images/eda2.jpg" alt="">
+                                    <div class="overlay-b">
+                                        <div class="overlay-inner">
+                                            <a href="assets/images/eda.jpg" class="fancybox fa fa-expand" title="The Sketch (1928)"></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="box-content project-detail">
+                                    <h2><a href="project-details.php">The Sketch (1928)</a></h2>
+                                    <p> “The Sketch” was painted in America and exhibited at the Seattle Fine Arts Society, which was composed of artists from Washington, Idaho, Utah, Oregon and Canada. It won the second highest honors. The first honors was won by one of my professors from the University of Washington.</p>
+                                </div>
+                            </div> <!-- /.project-item -->
+                            <div class="col-md-4 project-item mix architecture">
+                                <div class="project-thumb">
+                                    <img src="assets/images/eda3.jpg" alt="">
+                                    <div class="overlay-b">
+                                        <div class="overlay-inner">
+                                            <a href="assets/images/eda3.jpg" class="fancybox fa fa-expand" title="Mother and Daughter (1926)"></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="box-content project-detail">
+                                    <h2><a href="project-details.php">Mother and Daughter (1926)</a></h2>
+                                    <p> This is a portrait of Jane Noble Garott, the house-mother of the Filipino Club House in Seattle and her daughter, Jean. This work was created when the artist was studying in the US. Jean later married the artist.</p>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                </div>
+                        </div>
+                        
+
+
+
+
+
+    <!-- jQuery -->
+    <script src="assets/js/jquery-2.1.0.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="assets/js/popper.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- Plugins -->
+    <script src="assets/js/owl-carousel.js"></script>
+    <script src="assets/js/accordions.js"></script>
+    <script src="assets/js/datepicker.js"></script>
+    <script src="assets/js/scrollreveal.min.js"></script>
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <script src="assets/js/imgfix.min.js"></script> 
+    <script src="assets/js/slick.js"></script> 
+    <script src="assets/js/lightbox.js"></script> 
+    <script src="assets/js/isotope.js"></script> 
+    <script src="assets/js/quantity.js"></script>
+    
+    <!-- Global Init -->
+    <script src="assets/js/custom.js"></script>
+
+    <script>
+
+        $(function() {
+            var selectedClass = "";
+            $("p").click(function(){
+            selectedClass = $(this).attr("data-rel");
+            $("#portfolio").fadeTo(50, 0.1);
+                $("#portfolio div").not("."+selectedClass).fadeOut();
+            setTimeout(function() {
+              $("."+selectedClass).fadeIn();
+              $("#portfolio").fadeTo(50, 1);
+            }, 500);
+                
+            });
+        });
+
+    </script>
+
+  </body>
+
+</html>
