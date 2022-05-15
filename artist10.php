dc6ffa8061e9a62af404c44da20dd3fd2ef281c3
Karylle Manrique <manriquekarylle891@gmail.com>
2022-05-16 02:45:46 +0800

    artist10

---
+++ artist10.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <title>Filipino Arts Gallery - About Page</title>
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+
+    <link rel="stylesheet" href="assets/css/templatemo-hexashop.css">
+
+    <link rel="stylesheet" href="assets/css/owl-carousel.css">
+
+    <link rel="stylesheet" href="assets/css/lightbox.css">
+
+    </head>
+    
+    <body>
+    
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>  
+    <!-- ***** Preloader End ***** -->
+    
+    
+    <!-- ***** Header Area Start ***** -->
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+
+
+                        <!-- ***** Menu Start ***** -->
+                        <ul class="nav">
+                            <li class="scroll-to-section"><a href="index.php" class="active">Home</a></li>
+                            <li class="submenu">
+                                <a href="javascript:;">Information</a>
+                                <ul>
+                                    <li><a href="about.php">About Us</a></li>
+                                    <li><a href="history.php">History</a></li>
+                                    <li><a href="creators.php">About Creators</a></li>
+                                    <li><a href="contact.php">Contact Us</a></li>
+                                </ul>
+                            </li>
+                            <li class="submenu">
+                                <a href="javascript:;">Artists</a>
+                                <ul>
+                                    <li><a href="artist1.php">Fernando Amorsolo</a></li>
+                                    <li><a href="artist2.php">Juan Luna</a></li>
+                                    <li><a href="artist3.php">Guillermo Tolentino</a></li>
+                                    <li><a href="artist4.php">Benedicto Cabrera</a></li>
+                                    <li><a href="artist5.php">Vicente Manansala</a></li>
+                                    <li><a href="artist6.php">Victorio Edades</a></li>
+                                    <li><a href="artist7.php">Ang Kiukok</a></li>
+                                    <li><a href="artist8.php">Napoleon Abueva</a></li>
+                                    <li><a href="artist9.php">Hernando Ocampo</a></li>
+                                    <li><a href="artist10.php">Pacita Abad</a></li>
+                                </ul>
+                        </ul>        
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
+
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+</div>
+                </div>
+                </div>
+        <div class="content-wrapper">
+            <div class="inner-container container">
+                <div class="row">
+                    <div class="section-header col-md-12">
+                        <h2>Pacita Abad</h2>
+                        <span>(1946-2004)</span>
+                        <br>
+                        <br>
+                        <br>
+                        <p>Among other things, Pacita Abad is renowned for her eccentric use of color in her paintings. Some other unique features of her paintings included underwater scenes, along with wild animals and tropical flowers. She managed to highlight the constant changes and developments going on in the world around her. Her paintings are a reflection of her travels all over the world. After briefly studying painting in New York and Washington D.C., she went on to travel over 80 countries around the world. Her travels and her experiences with different people and cultures from various parts of the world had a major influence on her style of painting. In 1970s and 1980s, her work was majorly inspired by painters like Ben Shahn, which led her to create paintings which inspired political and social thought. Abad received several awards during her lifetime. In 1984, she became the first woman to win the Ten Outstanding Young Men (TOYM) Award. Additionally, she also won the D.C. Commission on the Arts Award in 1989 and 1990 and the Gwendolyn Caffritz Award in 1992.</p>
+                        <br>
+                        <br>
+                    </div>
+                    <div class="projects-holder">
+                        <div class="row">
+                            <div class="col-md-4 project-item mix design">
+                                <div class="project-thumb">
+                                    <img src="assets/images/abad1.jpg" alt="">
+                                    <div class="overlay-b">
+                                        <div class="overlay-inner">
+                                            <a href="assets/images/abad1.jpg" class="fancybox fa fa-expand" title="Underwater Wilderness Series (1986)"></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="box-content project-detail">
+                                    <h2><a href="project-details.php"> Underwater Wilderness Series (1986)</a></h2>
+                                    <p>Abad turned to the prosperous marine life of her province and the underwater landscapes she encountered during her many diving trips across the Philippines for inspiration in this painting. </p>
+                                </div>
+                            </div> <!-- /.project-item -->
+                            <div class="col-md-4 project-item mix nature">
+                                <div class="project-thumb">
+                                    <img src="assets/images/abad2.jpg" alt="">
+                                    <div class="overlay-b">
+                                        <div class="overlay-inner">
+                                            <a href="assets/images/abad2.jpg" class="fancybox fa fa-expand" title="Fly me to the moon (2000)"></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="box-content project-detail">
+                                    <h2><a href="project-details.php">Fly me to the moon (2000)</a></h2>
+                                    <p> This is a unique work . Pacita Abad's large-scale, richly colored paintings, textile collages, and mixed-media assemblages have long been revered in her native Philippines, and since her death in 2004 they have drawn international acclaim.</p>
+                                </div>
+                            </div> <!-- /.project-item -->
+                            <div class="col-md-4 project-item mix architecture">
+                                <div class="project-thumb">
+                                    <img src="assets/images/abad3.jpg" alt="">
+                                    <div class="overlay-b">
+                                        <div class="overlay-inner">
+                                            <a href="assets/images/abad3.jpg" class="fancybox fa fa-expand" title="I Put A Spell On You (1997)</"></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="box-content project-detail">
+                                    <h2><a href="project-details.php">I Put A Spell On You (1997)</a></h2>
+                                    <p> Brilliant is Abad’s approach to her art, as she juxtaposes a myriad of tones that come together in all their dissonance, concocting an intense, cohesive amalgamation of hues. Abad’s predilection for color can be traced, in a way, to her peripatetic life where she traveled the globe to take in the sights, sounds, cultures, and yes, colors, of the numerous places she visited.</p>
+                                </div>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                </div>
+                        </div>
+                        
+
+
+
+
+
+    <!-- jQuery -->
+    <script src="assets/js/jquery-2.1.0.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="assets/js/popper.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- Plugins -->
+    <script src="assets/js/owl-carousel.js"></script>
+    <script src="assets/js/accordions.js"></script>
+    <script src="assets/js/datepicker.js"></script>
+    <script src="assets/js/scrollreveal.min.js"></script>
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <script src="assets/js/imgfix.min.js"></script> 
+    <script src="assets/js/slick.js"></script> 
+    <script src="assets/js/lightbox.js"></script> 
+    <script src="assets/js/isotope.js"></script> 
+    <script src="assets/js/quantity.js"></script>
+    
+    <!-- Global Init -->
+    <script src="assets/js/custom.js"></script>
+
+    <script>
+
+        $(function() {
+            var selectedClass = "";
+            $("p").click(function(){
+            selectedClass = $(this).attr("data-rel");
+            $("#portfolio").fadeTo(50, 0.1);
+                $("#portfolio div").not("."+selectedClass).fadeOut();
+            setTimeout(function() {
+              $("."+selectedClass).fadeIn();
+              $("#portfolio").fadeTo(50, 1);
+            }, 500);
+                
+            });
+        });
+
+    </script>
+
+  </body>
+
+</html>
