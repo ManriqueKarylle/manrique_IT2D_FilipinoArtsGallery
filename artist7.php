95c6d83d0c12e5dc7531afe373bf043d80db15c6
Karylle Manrique <manriquekarylle891@gmail.com>
2022-05-16 02:43:32 +0800

    artist7

---
+++ artist7.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <title>Filipino Arts Gallery - About Page</title>
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+
+    <link rel="stylesheet" href="assets/css/templatemo-hexashop.css">
+
+    <link rel="stylesheet" href="assets/css/owl-carousel.css">
+
+    <link rel="stylesheet" href="assets/css/lightbox.css">
+
+    </head>
+    
+    <body>
+    
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>  
+    <!-- ***** Preloader End ***** -->
+    
+    
+    <!-- ***** Header Area Start ***** -->
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+
+
+                        <!-- ***** Menu Start ***** -->
+                        <ul class="nav">
+                            <li class="scroll-to-section"><a href="index.php" class="active">Home</a></li>
+                            <li class="submenu">
+                                <a href="javascript:;">Information</a>
+                                <ul>
+                                    <li><a href="about.php">About Us</a></li>
+                                    <li><a href="history.php">History</a></li>
+                                    <li><a href="creators.php">About Creators</a></li>
+                                    <li><a href="contact.php">Contact Us</a></li>
+                                </ul>
+                            </li>
+                            <li class="submenu">
+                                <a href="javascript:;">Artists</a>
+                                <ul>
+                                    <li><a href="artist1.php">Fernando Amorsolo</a></li>
+                                    <li><a href="artist2.php">Juan Luna</a></li>
+                                    <li><a href="artist3.php">Guillermo Tolentino</a></li>
+                                    <li><a href="artist4.php">Benedicto Cabrera</a></li>
+                                    <li><a href="artist5.php">Vicente Manansala</a></li>
+                                    <li><a href="artist6.php">Victorio Edades</a></li>
+                                    <li><a href="artist7.php">Ang Kiukok</a></li>
+                                    <li><a href="artist8.php">Napoleon Abueva</a></li>
+                                    <li><a href="artist9.php">Hernando Ocampo</a></li>
+                                    <li><a href="artist10.php">Pacita Abad</a></li>
+                                </ul>
+                        </ul>        
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
+
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+</div>
+        <div class="content-wrapper">
+            <div class="inner-container container">
+                <div class="row">
+                    <div class="section-header col-md-12">
+                        <h2>Ang Kiukok</h2>
+                        <span>(1931-2005)</span>
+                        <br>
+                        <br>
+                        <br>
+                        <p> A man of Chinese descent, Ang Kiukok is most famous for his expressive artwork. Almost all his paintings have one thing in common: an extremely disturbing subject matter. With a dynamic profile, his paintings have been known to depict death; crucifixions; and tortured and screaming figurines; which portray the dull and bleak reality of life. Kiukok was a critically as well as commercially successful painter and his paintings have had a high viewership. The decade of 1970s, when Philippines was under martial law, was the time when he made some of his extremely grueling paintings. Apart from his subject matter, Kiukok is renowned for his extremely unique style of painting, which was a mixture of Cubism, Expressionism and Surrealism. It was due to his style that he first gained fame and prominence in the 1960s. Ang Kiukok was named the National Artist for Visual Arts in the Philippines in the year 2001.</p>
+                        <br>
+                        <br>
+                    </div>
+                    <div class="projects-holder">
+                        <div class="row">
+                            <div class="col-md-4 project-item mix design">
+                                <div class="project-thumb">
+                                    <img src="assets/images/kiu1.jpg" alt="">
+                                    <div class="overlay-b">
+                                        <div class="overlay-inner">
+                                            <a href="assets/images/kiu1.jpg" class="fancybox fa fa-expand" title="Watermelon (1987)"></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="box-content project-detail">
+                                    <h2><a href="project-details.php"> Watermelon (1987)</a></h2>
+                                    <p>"Watermelon" by Ang kiukok is his most famous work. Vector abstract irregular polygon background with a triangular pattern in blue and pink color representing watermelon. </p>
+                                </div>
+                            </div> <!-- /.project-item -->
+                            <div class="col-md-4 project-item mix nature">
+                                <div class="project-thumb">
+                                    <img src="assets/images/kiu3.jpg" alt="">
+                                    <div class="overlay-b">
+                                        <div class="overlay-inner">
+                                            <a href="assets/images/kiu3.jpg" class="fancybox fa fa-expand" title="The Fishermen (1981)"></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="box-content project-detail">
+                                    <h2><a href="project-details.php">The Fishermen (1981)</a></h2>
+                                    <p> A faithful Catholic, Ang created works referencing parables from the bible. In the present Lot Fisherman, the reference could be alluded to the parable where Jesus asks fisherman with empty catches to cast their nets into the sea, and to their amazement the nets came up filled with a bountiful harvest from the sea.</p>
+                                </div>
+                            </div> <!-- /.project-item -->
+                            <div class="col-md-4 project-item mix architecture">
+                                <div class="project-thumb">
+                                    <img src="assets/images/kiu2.jpg" alt="">
+                                    <div class="overlay-b">
+                                        <div class="overlay-inner">
+                                            <a href="assets/images/kiu2.jpg" class="fancybox fa fa-expand" title="Angry Figure (1982)"></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="box-content project-detail">
+                                    <h2><a href="project-details.php">Angry Figure (1982)</a></h2>
+                                    <p> The painting of Kiukok “Angry Man” was published in 1982 when the MartialLaw was recently declared ended in 1981—but was not entirely true becausethe abuse of human rights had still continued afterwards. And as we all know, during the martial law, many people got killed and others diedbecause of poverty. Protesters were not allowed, or else, they would be sentto prison—worse is, killed secretly. </p>
+           
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                </div>
+                        </div>
+                        
+
+
+
+
+
+    <!-- jQuery -->
+    <script src="assets/js/jquery-2.1.0.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="assets/js/popper.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- Plugins -->
+    <script src="assets/js/owl-carousel.js"></script>
+    <script src="assets/js/accordions.js"></script>
+    <script src="assets/js/datepicker.js"></script>
+    <script src="assets/js/scrollreveal.min.js"></script>
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <script src="assets/js/imgfix.min.js"></script> 
+    <script src="assets/js/slick.js"></script> 
+    <script src="assets/js/lightbox.js"></script> 
+    <script src="assets/js/isotope.js"></script> 
+    <script src="assets/js/quantity.js"></script>
+    
+    <!-- Global Init -->
+    <script src="assets/js/custom.js"></script>
+
+    <script>
+
+        $(function() {
+            var selectedClass = "";
+            $("p").click(function(){
+            selectedClass = $(this).attr("data-rel");
+            $("#portfolio").fadeTo(50, 0.1);
+                $("#portfolio div").not("."+selectedClass).fadeOut();
+            setTimeout(function() {
+              $("."+selectedClass).fadeIn();
+              $("#portfolio").fadeTo(50, 1);
+            }, 500);
+                
+            });
+        });
+
+    </script>
+
+  </body>
+
+</html>
