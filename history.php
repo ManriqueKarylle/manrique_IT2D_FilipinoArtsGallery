25e65c2763e673299b75297e18a0b168fea444b0
Karylle Manrique <manriquekarylle891@gmail.com>
2022-05-16 02:32:52 +0800

    history

---
+++ history.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <title>History of Music and Arts - Page </title>
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+
+    <link rel="stylesheet" href="assets/css/templatemo-hexashop.css">
+
+    <link rel="stylesheet" href="assets/css/owl-carousel.css">
+
+    <link rel="stylesheet" href="assets/css/lightbox.css">
+
+
+
+    </head>
+    
+    <body>
+    
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>  
+    <!-- ***** Preloader End ***** -->
+    
+    
+    <!-- ***** Header Area Start ***** -->
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+
+                        <!-- ***** Menu Start ***** -->
+                        <ul class="nav">
+                            <li class="scroll-to-section"><a href="index.php" class="active">Home</a></li>
+                            <li class="submenu">
+                                <a href="javascript:;">Information</a>
+                                <ul>
+                                    <li><a href="about.php">About Us</a></li>
+                                    <li><a href="history.php">History</a></li>
+                                    <li><a href="creators.php">About Creators</a></li>
+                                    <li><a href="contact.php">Contact Us</a></li>
+                                </ul>
+                            </li>
+                            <li class="submenu">
+                                <a href="javascript:;">Artists</a>
+                                <ul>
+                                    <li><a href="artist1.php">Fernando Amorsolo</a></li>
+                                    <li><a href="artist2.php">Juan Luna</a></li>
+                                    <li><a href="artist3.php">Guillermo Tolentino</a></li>
+                                    <li><a href="artist4.php">Benedicto Cabrera</a></li>
+                                    <li><a href="artist5.php">Vicente Manansala</a></li>
+                                    <li><a href="artist6.php">Victorio Edades</a></li>
+                                    <li><a href="artist7.php">Ang Kiukok</a></li>
+                                    <li><a href="artist8.php">Napoleon Abueva</a></li>
+                                    <li><a href="artist9.php">Hernando Ocampo</a></li>
+                                    <li><a href="artist10.php">Pacita Abad</a></li>
+                                </ul>
+                        </ul>        
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
+
+    <!-- ***** Main Banner Area Start ***** -->
+    <div class="page-heading" id="top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="inner-content">
+                        <h2>History of Arts in Philippines</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ***** Main Banner Area End ***** -->
+
+<section class="section" id="history">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                <div class="left-images">
+                    <img src="assets/images/history.jpg" alt="">
+                    <br>
+                    <br>
+                    <img src="assets/images/history1.jpg" alt="">
+                    <br>
+                    <br>
+                    <img src="assets/images/history3.jpg" alt="">
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="right-content">
+                    </ul>
+                    <span>     The art of the Philippines is reflective of the diversity, richness and uniqueness of Filipino culture. It began during the pre-historic era which is signified by the various paintings and artworks found on the walls of the caves discovered throughout the region. However, Filipino Art, as we know today, began with colonization of the region by Spain in the early 16th century. The Spanish introduced Christianity to Philippines and art of the period was majorly influenced by religion. It thus reflects the religious propaganda through the country. Art played a significant role in the spread of Catholicism in the Philippines. From the 16th century till the 19th century, art in the Philippines was created primarily for the benefit and spread of Christianity. Change was seen in the early 19th century, when native people of the Philippines educated themselves. This change in education was also reflected in the art produced during that time. Among other things, there was a shift in focus from the church to the native culture of the country which included local landscapes, native fashion, jewelry and furniture. Another major change in Filipino art came during and after the Second World War. For many artists, the focus shifted from the native culture to the effects of the war. Painters started depicting battle scenes, death and the resulting suffering. Artists from the Philippines have continued to contribute to contemporary art of the world while experimenting with modern ways of expression. Here are the 10 most famous Filipino artists and their masterpieces.</span>
+                    <br>
+                    <br>
+                    <span>   Among other things, there was a shift in focus from the church to the native culture of the country which included local landscapes, native fashion, jewelry and furniture. Another major change in Filipino art came during and after the Second World War. For many artists, the focus shifted from the native culture to the effects of the war. Painters started depicting battle scenes, death and the resulting suffering. Artists from the Philippines have continued to contribute to contemporary art of the world while experimenting with modern ways of expression. Here are the 10 most famous Filipino artists and their masterpieces.</span>
+                    <br>
+                    <br>
+
+                </div>
+            </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+    
+    <!-- ***** Footer Start ***** -->
+    <footer>
+                <div class="col-lg-12">
+                    <div class="under-footer">
+                        <p>Copyright © 2022 Filipino Arts Gallery. All Rights Reserved. 
+                        
+                        <br>Template Credits:<a href="https://templatemo.com" target="_parent" title="free css templates"> TemplateMo</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    
+    <!-- ***** Footer ends ***** -->
+    
+
+    <!-- jQuery -->
+    <script src="assets/js/jquery-2.1.0.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="assets/js/popper.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- Plugins -->
+    <script src="assets/js/owl-carousel.js"></script>
+    <script src="assets/js/accordions.js"></script>
+    <script src="assets/js/datepicker.js"></script>
+    <script src="assets/js/scrollreveal.min.js"></script>
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <script src="assets/js/imgfix.min.js"></script> 
+    <script src="assets/js/slick.js"></script> 
+    <script src="assets/js/lightbox.js"></script> 
+    <script src="assets/js/isotope.js"></script> 
+    
+    <!-- Global Init -->
+    <script src="assets/js/custom.js"></script>
+
+    <script>
+
+        $(function() {
+            var selectedClass = "";
+            $("p").click(function(){
+            selectedClass = $(this).attr("data-rel");
+            $("#portfolio").fadeTo(50, 0.1);
+                $("#portfolio div").not("."+selectedClass).fadeOut();
+            setTimeout(function() {
+              $("."+selectedClass).fadeIn();
+              $("#portfolio").fadeTo(50, 1);
+            }, 500);
+                
+            });
+        });
+
+    </script>
+
+  </body>
+
+</html>
