3712a37554d4acade338f0104020d34e76cd9948
Karylle Manrique <manriquekarylle891@gmail.com>
2022-05-16 02:36:27 +0800

    artist1

---
+++ artist1.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <title>Filipino Arts Gallery - About Page</title>
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+
+    <link rel="stylesheet" href="assets/css/templatemo-hexashop.css">
+
+    <link rel="stylesheet" href="assets/css/owl-carousel.css">
+
+    <link rel="stylesheet" href="assets/css/lightbox.css">
+
+    </head>
+    
+    <body>
+    
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>  
+    <!-- ***** Preloader End ***** -->
+    
+    
+    <!-- ***** Header Area Start ***** -->
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+
+
+                        <!-- ***** Menu Start ***** -->
+                        <ul class="nav">
+                            <li class="scroll-to-section"><a href="index.php" class="active">Home</a></li>
+                            <li class="submenu">
+                                <a href="javascript:;">Information</a>
+                                <ul>
+                                    <li><a href="about.php">About Us</a></li>
+                                    <li><a href="history.php">History</a></li>
+                                    <li><a href="creators.php">About Creators</a></li>
+                                    <li><a href="contact.php">Contact Us</a></li>
+                                </ul>
+                            </li>
+                            <li class="submenu">
+                                <a href="javascript:;">Artists</a>
+                                <ul>
+                                    <li><a href="artist1.php">Fernando Amorsolo</a></li>
+                                    <li><a href="artist2.php">Juan Luna</a></li>
+                                    <li><a href="artist3.php">Guillermo Tolentino</a></li>
+                                    <li><a href="artist4.php">Benedicto Cabrera</a></li>
+                                    <li><a href="artist5.php">Vicente Manansala</a></li>
+                                    <li><a href="artist6.php">Victorio Edades</a></li>
+                                    <li><a href="artist7.php">Ang Kiukok</a></li>
+                                    <li><a href="artist8.php">Napoleon Abueva</a></li>
+                                    <li><a href="artist9.php">Hernando Ocampo</a></li>
+                                    <li><a href="artist10.php">Pacita Abad</a></li>
+                                </ul>
+                        </ul>        
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
+
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+   <div class="content-wrapper">
+            <div class="inner-container container">
+                <div class="row">
+                    <div class="section-header col-md-12">
+                        <h2>Fernando Amorsolo</h2>
+                        <span>(1892 - 1972)</span>
+                        <br>
+                        <br>
+                        <br>
+                        <p>     The Philippine artist Fernando Amorsolo (1892-1972) was a portraitist and painter of rural land scapes. He is best known for his craftsmanship and mastery in the use of light. 
+                        <br>
+                        <br>    Fernando Amorsolo was born May 30, 1892, in the Paco district of Manila. At 13 he was apprenticed to the noted Philippine artist Fabian de la Rosa, his mother's first cousin. In 1909 Amorsolo enrolled at the Liceo de Manila and then attended the fine-arts school at the University of the Philippines, graduating in 1914. After working three years as a commercial artist and part-time instructor at the university, he studied at the Escuela de San Fernando in Madrid. For seven months he sketched at the museums and on the streets of Madrid, experimenting with the use of light and color. That winter he went to New York and discovered the works of the postwar impressionists and cubists, who became the major influence on his works. On his return to Manila, he set up his own studio.</p>
+                        <br>
+                        <br>
+                    </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ***** About Area Ends ***** -->
+
+
+<br>
+                        <br>
+                    </div>
+                    <div class="projects-holder">
+                        <div class="row">
+                            <div class="col-md-4 project-item mix design">
+                                <div class="project-thumb">
+                                    <img src="assets/images/amor1.jpg" alt="">
+                                    <div class="overlay-b">
+                                        <div class="overlay-inner">
+                                            <a href="assets/images/amor1.jpg" class="fancybox fa fa-expand" title="Rice Planting (1951)"></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="box-content project-detail">
+                                    <h2><a href="project-details.php">Rice Planting (1951) </a></h2>
+                                    <p>The 'Planting Rice with Mayon Volcano' depicts happiness despite the difficulties of planting rice. The Filipino Villagers in their bright clothes and straw hats plant with a fresh and green landscape of plenty behind them is the peaceful flume of steam.</p>
+                                </div>
+                            </div> <!-- /.project-item -->
+                            <div class="col-md-4 project-item mix nature">
+                                <div class="project-thumb">
+                                    <img src="assets/images/amor2.jpg" alt="">
+                                    <div class="overlay-b">
+                                        <div class="overlay-inner">
+                                            <a href="assets/images/amor2.jpg" class="fancybox fa fa-expand" title="Fruit Gatherer (1950)"></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="box-content project-detail">
+                                    <h2><a href="project-details.php">Fruit Gatherer (1950)</a></h2>
+                                    <p>Mr. Fernando Amorsolo's 1950 oil on canvas painting "Fruit Gatherer" depicts a woman sitting under a bamboo tree with a winnowing basket full of fresh fruits. It also depicts the daily lives of a lady who lives in a province where fresh fruits, vegetables, and other edibles can be found.</p>
+                                </div>
+                            </div> <!-- /.project-item -->
+                            <div class="col-md-4 project-item mix architecture">
+                                <div class="project-thumb">
+                                    <img src="assets/images/amor3.jpg" alt="">
+                                    <div class="overlay-b">
+                                        <div class="overlay-inner">
+                                            <a href="assets/images/amor3.jpg" class="fancybox fa fa-expand" title="Sunday Morning Going to Town (1942)"></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="box-content project-detail">
+                                    <h2><a href="project-detail.php">Sunday Morning Going to Town (1942) </a></h2>
+                                    <p> Oil on canvas from the artist's Golden Period, is a romanticized scene of Filipino rural life painted in the midst of World War II. A family sets out together, leaving their hut for an unknown location.</p>
+
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                </div>
+                        </div>
+                        
+
+
+
+
+
+    <!-- jQuery -->
+    <script src="assets/js/jquery-2.1.0.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="assets/js/popper.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- Plugins -->
+    <script src="assets/js/owl-carousel.js"></script>
+    <script src="assets/js/accordions.js"></script>
+    <script src="assets/js/datepicker.js"></script>
+    <script src="assets/js/scrollreveal.min.js"></script>
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <script src="assets/js/imgfix.min.js"></script> 
+    <script src="assets/js/slick.js"></script> 
+    <script src="assets/js/lightbox.js"></script> 
+    <script src="assets/js/isotope.js"></script> 
+    <script src="assets/js/quantity.js"></script>
+    
+    <!-- Global Init -->
+    <script src="assets/js/custom.js"></script>
+
+    <script>
+
+        $(function() {
+            var selectedClass = "";
+            $("p").click(function(){
+            selectedClass = $(this).attr("data-rel");
+            $("#portfolio").fadeTo(50, 0.1);
+                $("#portfolio div").not("."+selectedClass).fadeOut();
+            setTimeout(function() {
+              $("."+selectedClass).fadeIn();
+              $("#portfolio").fadeTo(50, 1);
+            }, 500);
+                
+            });
+        });
+
+    </script>
+
+  </body>
+
+</html>
