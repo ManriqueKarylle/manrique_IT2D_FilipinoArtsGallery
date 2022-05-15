fd40c1b8921d243e843348f89198fb73e2ba13c9
Karylle Manrique <manriquekarylle891@gmail.com>
2022-05-16 02:40:36 +0800

    artist5

---
+++ artist5.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <title>Filipino Arts Gallery - About Page</title>
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+
+    <link rel="stylesheet" href="assets/css/templatemo-hexashop.css">
+
+    <link rel="stylesheet" href="assets/css/owl-carousel.css">
+
+    <link rel="stylesheet" href="assets/css/lightbox.css">
+
+    </head>
+    
+    <body>
+    
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>  
+    <!-- ***** Preloader End ***** -->
+    
+    
+    <!-- ***** Header Area Start ***** -->
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+
+
+                        <!-- ***** Menu Start ***** -->
+                        <ul class="nav">
+                            <li class="scroll-to-section"><a href="index.php" class="active">Home</a></li>
+                            <li class="submenu">
+                                <a href="javascript:;">Information</a>
+                                <ul>
+                                    <li><a href="about.php">About Us</a></li>
+                                    <li><a href="history.php">History</a></li>
+                                    <li><a href="creators.php">About Creators</a></li>
+                                    <li><a href="contact.php">Contact Us</a></li>
+                                </ul>
+                            </li>
+                            <li class="submenu">
+                                <a href="javascript:;">Artists</a>
+                                <ul>
+                                    <li><a href="artist1.php">Fernando Amorsolo</a></li>
+                                    <li><a href="artist2.php">Juan Luna</a></li>
+                                    <li><a href="artist3.php">Guillermo Tolentino</a></li>
+                                    <li><a href="artist4.php">Benedicto Cabrera</a></li>
+                                    <li><a href="artist5.php">Vicente Manansala</a></li>
+                                    <li><a href="artist6.php">Victorio Edades</a></li>
+                                    <li><a href="artist7.php">Ang Kiukok</a></li>
+                                    <li><a href="artist8.php">Napoleon Abueva</a></li>
+                                    <li><a href="artist9.php">Hernando Ocampo</a></li>
+                                    <li><a href="artist10.php">Pacita Abad</a></li>
+                                </ul>
+                        </ul>        
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
+
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+</div>
+         <div class="content-wrapper">
+            <div class="inner-container container">
+                <div class="row">
+                    <div class="section-header col-md-12">
+                        <h2>Vicente Manansala</h2>
+                        <span>(1942-2013)</span>
+                        <br>
+                        <br>
+                        <br>
+                        <p> Vicente Manansala was a prodigious artist who received a six-month grant from UNESCO and was invited to study at the Ecole des Beaux-Arts in Quebec, Canada. He is most commonly known for his Cubist paintings, prints and illustration. He was a member of the Thirteen Moderns, which was led by Victorio Edades. As a neo-realist, he became one of the few artists who were responsible for the modernist movement in the country. Primarily, his work was focused on the issues and the problems of the world. Along with this, the Second World War had a great impact on his paintings. National culture, social environment, identity and native sensibility became the epicenter of his works. Along with this, he also worked on some abstract pieces but his main focus remained on using art as a social commentary. Manansala held his first solo exhibition in the year 1951 at the Manila Hotel. His work had an immense influence on the next generation of painters which included Manuel Baldemor and Angelito Antonio. The Honolulu Museum of Art, the Lopez Memorial Museum, Manila and the Philippine Center in New York hold some of his most famous works. He was posthumously awarded the Order of National Artists of the Philippines in Visual Arts (Painting) in the year 1981.</p>
+                        <br>
+                        <br>
+                    </div>
+                    <div class="projects-holder">
+                        <div class="row">
+                            <div class="col-md-4 project-item mix design">
+                                <div class="project-thumb">
+                                    <img src="assets/images/mana1.jpg" alt="">
+                                    <div class="overlay-b">
+                                        <div class="overlay-inner">
+                                            <a href="assets/images/mana1.jpg" class="fancybox fa fa-expand" title="Madonna of the Slums (1950)"></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="box-content project-detail">
+                                    <h2><a href="project-details.php"> Madonna of the Slums (1950) </a></h2>
+                                    <p>Madonna of the Slums is a portrayal of a mother and child from the countryside who became urban shanty residents once in the city. In his Jeepneys, Manansala combined the elements of provincial folk culture with the congestion issues of the city.</p>
+                                </div>
+                            </div> <!-- /.project-item -->
+                            <div class="col-md-4 project-item mix nature">
+                                <div class="project-thumb">
+                                    <img src="assets/images/mana2.jpg" alt="">
+                                    <div class="overlay-b">
+                                        <div class="overlay-inner">
+                                            <a href="assets/images/mana2.jpg" class="fancybox fa fa-expand" title="The Bird Seller (1976)"></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="box-content project-detail">
+                                    <h2><a href="project-details.php">The Bird Seller (1976)</a></h2>
+                                    <p> The Bird Seller reflects all these unique qualities and influences, depicting a local vendor of tropical birds crouched under the heavy burden of his pole lying across his shoulders. Around him sway a myriad of geometric bird cages, creating layers of textural steppes across the pictorial surface.</p>
+                                </div>
+                            </div> <!-- /.project-item -->
+                            <div class="col-md-4 project-item mix architecture">
+                                <div class="project-thumb">
+                                    <img src="assets/images/mana3.jpg" alt="">
+                                    <div class="overlay-b">
+                                        <div class="overlay-inner">
+                                            <a href="assets/images/mana3.jpg" class="fancybox fa fa-expand" title="Jeepneys (1951)"></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="box-content project-detail">
+                                    <h2><a href="project-details.php">Jeepneys (1951)</a></h2>
+                                    <p> Jeepneys is a composition made in enamel on masonite board (fiberboard), measuring 51 x 59 cm. It shows the mastery of modernism within the Philippine context by the early 1950s modernists.</p>
+
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                                </div>
+                        </div>
+                        
+
+
+
+
+
+    <!-- jQuery -->
+    <script src="assets/js/jquery-2.1.0.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="assets/js/popper.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- Plugins -->
+    <script src="assets/js/owl-carousel.js"></script>
+    <script src="assets/js/accordions.js"></script>
+    <script src="assets/js/datepicker.js"></script>
+    <script src="assets/js/scrollreveal.min.js"></script>
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <script src="assets/js/imgfix.min.js"></script> 
+    <script src="assets/js/slick.js"></script> 
+    <script src="assets/js/lightbox.js"></script> 
+    <script src="assets/js/isotope.js"></script> 
+    <script src="assets/js/quantity.js"></script>
+    
+    <!-- Global Init -->
+    <script src="assets/js/custom.js"></script>
+
+    <script>
+
+        $(function() {
+            var selectedClass = "";
+            $("p").click(function(){
+            selectedClass = $(this).attr("data-rel");
+            $("#portfolio").fadeTo(50, 0.1);
+                $("#portfolio div").not("."+selectedClass).fadeOut();
+            setTimeout(function() {
+              $("."+selectedClass).fadeIn();
+              $("#portfolio").fadeTo(50, 1);
+            }, 500);
+                
+            });
+        });
+
+    </script>
+
+  </body>
+
+</html>
